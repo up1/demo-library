b81d934757bc61b5a5adbec7e64faa963ef55e60
up1 <somkiat.p@gmail.com>
2022-02-16 12:27:25 +0700

    Fix name

--- app/src/main/java/com/example/modulea/MainActivity.kt
+++ app/src/main/java/com/example/modulea/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.modulea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.hello_module.Hello
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var hello =  Hello()
-        print(hello.sayHi("demo name"))
+//        var hello =  Hello()
+//        print(hello.sayHi("demo name"))
     }
 }
